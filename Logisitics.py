1260d73aec41689b4a388c89f48e6f86ef5a9514
Frans Grau <fransgrau01@gmail.com>
2022-10-03 14:40:46 +0100

    1

--- Logisitics.py
+++ Logisitics.py
@@ -7,5 +7,5 @@ st.title('Logistics')
 
 link = "https://github.com/anacmqui/Toys_models-Project-2/blob/main/Q3_Logisitics_P2.ipynb"
 
-df_Logistics01 = pd.read_html
-df_Logistics01
+df_Logistics01 = pd.read_html(link)
+df_Logistics01 
