63b9f25bb221eb72894bfbc9ee6d599a53896522
anacmqui <anacmquintino@gmail.com>
2022-10-04 16:46:35 +0200

    added filter

--- str-mainmenu.py
+++ str-mainmenu.py
@@ -8,19 +8,9 @@ import numpy as np
 import datetime
 import mysql.connector
 
-primaryColor = '#77FFE3'
 
 
-st.title('Project 2!')
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "Select company's department",
-    ["Sales", "Finance","Logistics", "HR"],
-    )
-
-
-connection2 = mysql.connector.connect(user = 'toyscie', password = 'WILD4Rdata!', host = '51.68.18.102', port = '23456', database = 'toys_and_models')
 
 #Query 1 - Define turnover of countries of last 2 months
 query_fin = '''select customers.country, month(orders.orderDate) as 'Month', sum(orderdetails.quantityOrdered*orderdetails.priceEach) as Turnover
@@ -31,7 +21,7 @@ join customers
 on orders.customerNumber = customers.customerNumber
 where status <>'Cancelled' and orders.orderDate>=date_sub(curdate(), interval 2 month)
 group by country, monthname(orders.orderDate) desc;'''
-dffin = pd.read_sql_query(query_fin, con=connection2)
+
 #dffin
 
 query_fin2 = '''with amount_ordered as (select orders.customerNumber, sum(orderdetails.priceEach*orderdetails.quantityOrdered) as final_ordered from orderdetails
@@ -48,8 +38,7 @@ on amount_ordered.customerNumber = amount_paid.customerNumber
 having to_be_paid <> 0
 order by to_be_paid desc
 ;'''
-dffin2 = pd.read_sql_query(query_fin2, con=connection2)
-dffin2['customerNumber'] = dffin2['customerNumber'].astype(str)
+
 
 #Contact information of the clients overdue
 query_fin3='''select customers.customerNumber as Customer_Number, customerName as Customer_Name, country as Country, concat(contactFirstName, ' ', contactLastName) as Contact_person, phone, count(orderNumber) as Total_orders from customers
@@ -62,9 +51,67 @@ group by customers.customerNumber
 ;'''
 
 #Table with contact info
-dffin3 = pd.read_sql_query(query_fin3, con=connection2)
-dffin3.set_index('Customer_Number', drop = True)
 
+query_sales = '''with productline_sales as (
+select productLine,
+month(orderDate) order_month,
+year(orderDate) order_year,
+DATE_FORMAT(orderDate, "%M %Y") as month_year,
+round(sum(quantityOrdered),0) total_orders
+from orders
+inner join orderdetails using (orderNumber)
+inner join products using (productCode)
+group by productLine, order_year, order_month
+)
+select productline,  total_orders, order_month, order_year, month_year, LAG(total_orders, 1) over (
+partition by productLine, order_month
+order by productLine, month_year
+) prev_year_order_total , (((total_orders - (LAG(total_orders, 1) over (
+partition by productLine, order_month
+order by  productLine, month_year
+)))/(LAG(total_orders, 1) over (
+partition by productLine
+order by  productLine, month_year ASC
+))) *100) as growth
+from productline_sales
+order by  productLine, month_year'''
+    
+
+query_logistics = '''select sum(products.quantityInStock), products.productName
+from products
+join orderdetails
+on products.productCode = orderdetails.productCode
+join orders
+on orderdetails.orderNumber = orders.orderNumber
+where not orders.status ='Cancelled'
+group by products.productName
+order by sum(orderdetails.quantityOrdered) desc limit 0,5'''
+
+connection2 = mysql.connector.connect(user = 'toyscie', password = 'WILD4Rdata!', host = '51.68.18.102', port = '23456', database = 'toys_and_models')    
+#connection3 = mysql.connector.connect(user = 'toyscie', password = 'WILD4Rdata!', host = '51.68.18.102', port = '23456', database = 'toys_and_models')
+#connection = 'mysql://toyscie:WILD4Rdata!@51.68.18.102:23456/toys_and_models'
+#sql_engine = sql.create_engine(connection) 
+
+ 
+dffin = pd.read_sql_query(query_fin, con=connection2)    
+dffin2 = pd.read_sql_query(query_fin2, con=connection2)
+dffin2['customerNumber'] = dffin2['customerNumber'].astype(str)        
+dffin3 = pd.read_sql_query(query_fin3, con=connection2)
+dffin3.set_index('Customer_Number', drop = True)    
+dfSales = pd.read_sql(query_sales, con=connection2)
+dfLog = pd.read_sql_query(query_logistics, con=connection2)    
+
+primaryColor = '#77FFE3'
+    
+st.title('Project 2!')
+
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "Select company's department",
+    ["Sales", "Finance","Logistics", "HR"],
+    )    
+    
 
 # Query 1 plot
 if add_selectbox == 'Finance':
@@ -90,34 +137,9 @@ if add_selectbox == 'Finance':
 elif add_selectbox == 'Sales':
     st.markdown('''Welcome to *Sales*''')
     
-    connection = 'mysql://toyscie:WILD4Rdata!@51.68.18.102:23456/toys_and_models'
-    sql_engine = sql.create_engine(connection)
-    query_sales = '''with productline_sales as (
-        select productLine,
-            monthname(orderDate) order_month,
-                    year(orderDate) order_year,
-                    DATE_FORMAT(orderDate, "%M %Y") as month_year,
-                    round(sum(quantityOrdered),0) total_orders
-            from orders
-            inner join orderdetails using (orderNumber)
-            inner join products using (productCode)
-            group by productLine, order_year, order_month
-            )
-    select productline,  total_orders, order_month, order_year, month_year, LAG(total_orders, 1) over (
-        partition by productLine, order_month
-            order by productLine, month_year
-            ) prev_year_order_total , (((total_orders - (LAG(total_orders, 1) over (
-        partition by productLine, order_month
-            order by  productLine, month_year
-            )))/(LAG(total_orders, 1) over (
-        partition by productLine
-            order by  productLine, month_year ASC
-            ))) *100) as growth
-    from productline_sales
-    order by  productLine, month_year'''
-    
-    connection3 = mysql.connector.connect(user = 'toyscie', password = 'WILD4Rdata!', host = '51.68.18.102', port = '23456', database = 'toys_and_models')
-    dfSales = pd.read_sql(query_sales, con=connection3)
+    #connection = 'mysql://toyscie:WILD4Rdata!@51.68.18.102:23456/toys_and_models'
+    #sql_engine = sql.create_engine(connection)
+   
 
     fig01, ax = plt.subplots(figsize = (15, 5))
     dfS = dfSales.groupby('productline').mean()
@@ -133,21 +155,21 @@ elif add_selectbox == 'Sales':
     print('Total orders in 2020')
     print(dfSales[dfSales['order_year']==2020].groupby('productline').total_orders.sum())
     print('')
-    st.text('Total orders in 2020')
-    st.table(dfSales[dfSales['order_year']==2020].groupby('productline').total_orders.sum())
+    #st.text('Total orders in 2020')
+    #st.table(dfSales[dfSales['order_year']==2020].groupby('productline').total_orders.sum())
     
     print('Total orders in 2021')
     print(dfSales[dfSales['order_year']==2021].groupby('productline').total_orders.sum())
     print('')
-    st.text(" ")
-    st.text("Total orders 2021")
-    st.table(dfSales[dfSales['order_year']==2021].groupby('productline').total_orders.sum())
+    #st.text(" ")
+    #st.text("Total orders 2021")
+    #st.table(dfSales[dfSales['order_year']==2021].groupby('productline').total_orders.sum())
 
     print('Total orders all years')
     print(dfSales.groupby('productline').total_orders.sum())
-    st.text(" ")
-    st.text("Total orders all years")
-    st.table(dfSales.groupby('productline').total_orders.sum())
+    #st.text(" ")
+    #st.text("Total orders all years")
+    #st.table(dfSales.groupby('productline').total_orders.sum())
 
     data2020 = dfSales[dfSales['order_year']==2020].groupby('productline').total_orders.sum()
     data2021 = dfSales[dfSales['order_year']==2021].groupby('productline').total_orders.sum()
@@ -166,25 +188,25 @@ elif add_selectbox == 'Sales':
     fig02,  ax = plt.subplots(figsize =(10, 5))
     plt.pie(data2020, labels = labels, colors = colors1, autopct='%.0f%%')
     #plt.show()
-    st.text(" ")
-    st.text("Plot 2020")
-    st.pyplot(fig02)
+    #st.text(" ")
+    #st.text("Plot 2020")
+    #st.pyplot(fig02)
     
     #Year 2021
     print('Plot 2021')
     fig03, ax = plt.subplots(figsize = (10,5))
     plt.pie(data2021, labels = labels, colors = colors1, autopct='%.0f%%')
     #plt.show()
-    st.text(" ")
-    st.text("Plot 2021")
-    st.pyplot(fig03)
+    #st.text(" ")
+    #st.text("Plot 2021")
+    #st.pyplot(fig03)
 
     #Year 2022
     fig04, ax = plt.subplots(figsize =(5,5))
     plt.pie(data2022, labels= labels, colors= colors1, autopct='%.0f%%' )
-    st.text(" ")
-    st.text("Plot 2022")
-    st.pyplot(fig04)
+    #st.text(" ")
+    #st.text("Plot 2022")
+    #st.pyplot(fig04)
 
     #All years
     fig05, ax = plt.subplots(figsize = (5,5))
@@ -193,39 +215,33 @@ elif add_selectbox == 'Sales':
     st.text("Plot All")
     st.pyplot(fig05)
     
-    dfSales[dfSales['productline']=='Classic Cars']
-    dfCC = dfSales[dfSales['productline']=='Classic Cars']
+    #dfSales[dfSales['productline']=='Classic Cars']
+    
+    options = st.selectbox('Choose the type of product:', ['Classic Cars', 'Vintage Cars', 'Planes', 'Motorcycles','Ships','Trains','Trucks and Buses'])
+    
+    
+    #dfCC = dfSales[dfSales['productline']==options]
 
     fig07, ax = plt.subplots(figsize = (15, 5))
-    sns.barplot(data=dfCC, x='order_month', y="total_orders", hue="order_year", ci=None)
+    sns.barplot(data=dfSales[dfSales['productline']==options], x='order_month', y="total_orders", hue="order_year", ci=None)
     ax.set_ylabel('Orders')
     ax.set_xlabel('Month')
     ax.set_title('Monthly order growth for Classic Cars')
     plt.legend(loc='upper right', title='Year')
     st.pyplot(fig07)
 
-    fig08, ax = plt.subplots(figsize =(15,5))
-    sns.set(rc={'figure.figsize':(12,5)})
-    sns.barplot(data=dfCC, x='order_month', y="total_orders", hue="order_year", ci=None)
-    plt.legend(loc='upper right', title='Year')
-    st.pyplot(fig08)
+  
+    
+    #fig08, ax = plt.subplots(figsize =(15,5))
+    #sns.set(rc={'figure.figsize':(12,5)})
+    #sns.barplot(data=dfCC, x='order_month', y="total_orders", hue="order_year", ci=None)
+    #plt.legend(loc='upper right', title='Year')
+    #st.pyplot(fig08)
 
 elif add_selectbox == 'Logistics':
     st.markdown('''Hi, _this_ is **Logistics**''')
-    connection = 'mysql://toyscie:WILD4Rdata!@51.68.18.102:23456/toys_and_models'
-    sql_engine = sql.create_engine(connection) 
-
-    query_logistics = '''select sum(products.quantityInStock), products.productName
-    from products
-    join orderdetails
-    on products.productCode = orderdetails.productCode
-    join orders
-    on orderdetails.orderNumber = orders.orderNumber
-    where not orders.status ='Cancelled'
-    group by products.productName
-    order by sum(orderdetails.quantityOrdered) desc limit 0,5'''
-
-    dfLog = pd.read_sql_query(query_logistics, sql_engine)
+    
+    
     print(dfLog)
 
 
