5ef9269d139ea127b211983f8f2fad88c5d58687
Frans Grau <fransgrau01@gmail.com>
2022-10-04 14:14:48 +0100

    add logisitics

--- str-mainmenu.py
+++ str-mainmenu.py
@@ -212,5 +212,25 @@ elif add_selectbox == 'Sales':
 
 elif add_selectbox == 'Logistics':
     st.markdown('''Hi, _this_ is **Logistics**''')
+    connection = 'mysql://toyscie:WILD4Rdata!@51.68.18.102:23456/toys_and_models'
+    sql_engine = sql.create_engine(connection) 
+
+    query_logistics = '''select sum(products.quantityInStock), products.productName
+    from products
+    join orderdetails
+    on products.productCode = orderdetails.productCode
+    join orders
+    on orderdetails.orderNumber = orders.orderNumber
+    where not orders.status ='Cancelled'
+    group by products.productName
+    order by sum(orderdetails.quantityOrdered) desc limit 0,5'''
+
+    dfLog = pd.read_sql_query(query_logistics, sql_engine)
+    print(dfLog)
+
+
+    st.title('Logistics')
+    st.table(dfLog)
+    
 else:
     st.markdown('''Hi, _this_ is **HR**''')
